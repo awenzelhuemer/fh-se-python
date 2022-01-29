e3fb7e071465f8f971514baafaa6fcdab8da6971
awenzelhuemer <43669899+Superandi1234@users.noreply.github.com>
2022-01-29 10:38:26 +0100

    Add exercise 7

--- exercise/Part2/Ue02/src/app.py
+++ exercise/Part2/Ue02/src/app.py
@@ -7,7 +7,7 @@ from pandas import DataFrame
 
 app = Flask(__name__)
 
-output_directory = "files"
+output_directory = "uploaded_files"
 filetypes = ('.csv', '.txt', '.json', '.db')
 
 
@@ -43,6 +43,9 @@ def parse_json(filename) -> DataFrame:
 
 
 def parse_file(name: str, file_type: str) -> DataFrame:
+    if not os.path.isfile(name):
+        raise ValueError('File does not exist')
+
     if file_type == '.csv' or file_type == '.txt':
         return parse_csv(name, separator=',')
     elif file_type == '.json':
@@ -78,12 +81,16 @@ def export_file(filename: str):
     return send_from_directory('temp', target_file_name)
 
 
-@app.route('/details/<filename>', methods=['GET'])
-def details(filename: str):
-    full_filename = os.path.join(output_directory, filename)
-    file_path = pathlib.Path(full_filename)
-    df = parse_file(full_filename, file_path.suffix)
-    return render_template('details.html', filename=filename, data_frame=df)
+@app.route('/<filename>', methods=['GET'])
+def get_file(filename: str):
+    try:
+        full_filename = os.path.join(output_directory, filename)
+        file_path = pathlib.Path(full_filename)
+        df = parse_file(full_filename, file_path.suffix)
+        return render_template('details.html', filename=filename, data_frame=df)
+    except BaseException as e:
+        print("Error: ", e)
+        return redirect("/")
 
 
 @app.route('/delete-col/<filename>/<index>', methods=['GET'])
@@ -94,7 +101,7 @@ def delete_col(filename: str, index: str):
         delete_file(filename)
         return redirect("/")
 
-    return redirect(f'/details/{filename}')
+    return redirect(f'/{filename}')
 
 
 @app.route('/delete-row/<filename>/<index>', methods=['GET'])
@@ -105,31 +112,7 @@ def delete_row(filename: str, index: str):
         delete_file(filename)
         return redirect("/")
 
-    return redirect(f'/details/{filename}')
-
-
-def delete_col_or_row(filename: str, index: int, is_row: bool):
-    full_filename = os.path.join(output_directory, filename)
-    file_path = pathlib.Path(full_filename)
-    file_type = file_path.suffix
-    df = parse_file(full_filename, file_path.suffix)
-
-    if is_row:
-        df.drop([index], inplace=True)
-    else:
-        df.drop(df.columns[index], axis=1, inplace=True)
-
-    # Get table name if necessary
-    if file_type == '.db':
-        conn = sqlite3.connect(full_filename)
-        table_name = get_sqlite_table_name(full_filename, conn)
-        conn.close()
-    else:
-        table_name = None
-
-    save_file(df, output_directory, filename, file_type, table_name)
-
-    return df
+    return redirect(f'/{filename}')
 
 
 @app.route('/', methods=['POST'])
@@ -158,6 +141,53 @@ def add_file():
     return redirect('/')
 
 
+@app.route('/edit/<filename>/<row_index>/<column_index>', methods=['GET'])
+def edit_entry(filename: str, row_index: str, column_index: str):
+    full_filename = os.path.join(output_directory, filename)
+    file_path = pathlib.Path(full_filename)
+    df = parse_file(full_filename, file_path.suffix)
+
+    value = df.iloc[int(row_index), int(column_index)]
+
+    return render_template("edit.html", row_index=row_index, column_index=column_index, filename=filename, value=value)
+
+
+@app.route('/edit/<filename>/<row_index>/<column_index>', methods=['POST'])
+def update_entry(filename: str, row_index: str, column_index: str):
+    set_entry(filename, row_index, column_index, request.form["value"])
+    return redirect(f'/{filename}')
+
+
+@app.route('/delete/<filename>/<row_index>/<column_index>', methods=['GET'])
+def delete_entry(filename: str, row_index: str, column_index: str):
+    set_entry(filename, row_index, column_index, None)
+    return redirect(f'/{filename}')
+
+
+def delete_col_or_row(filename: str, index: int, is_row: bool):
+    full_filename = os.path.join(output_directory, filename)
+    file_path = pathlib.Path(full_filename)
+    file_type = file_path.suffix
+    df = parse_file(full_filename, file_path.suffix)
+
+    if is_row:
+        df.drop([index], inplace=True)
+    else:
+        df.drop(df.columns[index], axis=1, inplace=True)
+
+    # Get table name if necessary
+    if file_type == '.db':
+        conn = sqlite3.connect(full_filename)
+        table_name = get_sqlite_table_name(full_filename, conn)
+        conn.close()
+    else:
+        table_name = None
+
+    save_file(df, output_directory, filename, file_type, table_name)
+
+    return df
+
+
 def save_file(df: DataFrame, output_dir: str, filename: str, file_type: str, table_name: str = None):
     if file_type == '.csv' or file_type == '.txt':
         df.to_csv(os.path.join(output_dir, filename), index=False)
@@ -174,30 +204,7 @@ def save_file(df: DataFrame, output_dir: str, filename: str, file_type: str, tab
         conn.close()
 
 
-@app.route('/edit/<filename>/<row_index>/<column_index>', methods=['GET'])
-def edit_entry(filename: str, row_index: str, column_index: str):
-    full_filename = os.path.join(output_directory, filename)
-    file_path = pathlib.Path(full_filename)
-    df = parse_file(full_filename, file_path.suffix)
-
-    value = df.iloc[int(row_index), int(column_index)]
-
-    return render_template("edit.html", row_index=row_index, column_index=column_index, filename=filename, value=value)
-
-
-@app.route('/edit/<filename>/<row_index>/<column_index>', methods=['POST'])
-def update_entry(filename: str, row_index: str, column_index: str):
-    update_entry(filename, row_index, column_index, request.form["value"])
-    return redirect(f'/details/{filename}')
-
-
-@app.route('/delete-value/<filename>/<row_index>/<column_index>', methods=['GET'])
-def delete_entry(filename: str, row_index: str, column_index: str):
-    set_entry(filename, row_index, column_index, "null")
-    return redirect(f'/details/{filename}')
-
-
-def set_entry(filename: str, row_index: str, column_index: str, value: str):
+def set_entry(filename: str, row_index: str, column_index: str, value: str | None):
     full_filename = os.path.join(output_directory, filename)
     file_path = pathlib.Path(full_filename)
     file_type = file_path.suffix
